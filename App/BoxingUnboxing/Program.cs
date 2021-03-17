c73f455650fb4dbdc28280dec257d627b812486a
Дмитрий <dvr07@yandex.ru>
2021-03-17 19:44:17 +0300

    Проблемы Boxing / Unboxing

---
+++ App/BoxingUnboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 123;
+
+            // Создается новый объект в куче и значение копируется
+            object o = i;
+
+            i = 456; // Значение в o при этом не меняется
+            int j = (int)o;
+
+            // Изменения i не повлияли на o
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("o = {0}", o);
+            Console.WriteLine("j = {0}", j);
+
+            Print(i); // незаметный боксинг
+
+            Console.ReadLine();
+        }
+
+        public static void Print(object obj)
+        {
+            Console.WriteLine("Незаметный боксинг при вызове Print. obj = {0}", obj);
+        }
+    }
+}
